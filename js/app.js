97ed076498c5b503e7b73d91bc49728aeb4b39da
Jose Vte. Díaz Mora <74594028+JVDMKKHHPMY@users.noreply.github.com>
2021-02-15 17:22:40 +0100

    Cogiendo documentación

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+let promise = app.get("/");
+
+promise.then((req) => res);
+
+app.listen(3000, () => console.log("SERVIDOR ESCUCHANDO POR EL PUERTO 3000"));
